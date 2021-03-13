2fc34f2a96465a48959f166a43132993c43b7c87
mateuszlazar <mateuszlazar@outlook.com>
2021-03-13 15:56:03 +0100

    add shell script to deploy with gh pages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 set -e # stop on error
 
-echo bulid client ...
+echo -e '\n Deployment in progress... \n'
 
 BUILD_BRANCH='main'
-DEPLOY_BRANCH_BRANCH='gh-pages'
+DEPLOY_BRANCH='gh-pages'
 DIR=$(dirname "$(readlink -f "$0")")
 
-# echo "$DIR"
-# cd "$DIR"
-# mkdir -p "$DIR"/build
-# cp -r "$DIR"/dist/* "$DIR"/build/
-
 git fetch -f . $BUILD_BRANCH:$DEPLOY_BRANCH
-git checkout $DEPLOY_BRANCH
+git checkout $DEPLOY_BRANCH || git checkout $BUILD_BRANCH
 git add -f dist
-git commit -m "deploy to $DEPLOY_BRANCH"
+git commit -m "deploy to $DEPLOY_BRANCH" || git checkout $BUILD_BRANCH
 git push origin `git subtree split --prefix dist`:$DEPLOY_BRANCH --force
 git checkout $BUILD_BRANCH
 
-echo All done!
+echo -e '\n \U0002705 Deployment has been finished. \n'
